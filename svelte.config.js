2e085c2b355dee03a7421b54cbb3255c0186dd12
Glory Katende <katendeglory@gmail.com>
2021-06-18 21:13:17 +0200

    Initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,8 @@
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+import adapter from '@sveltejs/adapter-static';
 
-export default config;
+export default {
+	kit: {
+		adapter: adapter({})
+	},
+};
